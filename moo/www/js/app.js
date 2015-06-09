12e47cfecdb57e9a43d86172ae90db24cac2a0c4
AVatch <avatchinsky@outlook.com>
2015-06-08 21:41:35 -0400

    signup modal hooked up to event listeners

--- moo/www/js/app.js
+++ moo/www/js/app.js
@@ -372,12 +372,14 @@ angular.module('moo', ['ionic', 'LocalStorageModule'])
       }
     }, function(e){console.log(e);});
 
-
+  
+  // Handle Authentication
         
     $scope.login = function(user){
       Authentication.authenticateUser(user).then(function(s){
         if(s.status == 200){
           Authentication.cacheToken(s.data);
+          $scope.closeLoginModal();
           $state.go('threads');
         }else if(s.status == 400){
           console.log("Bad Credentials");
@@ -389,23 +391,28 @@ angular.module('moo', ['ionic', 'LocalStorageModule'])
     };
     
     $scope.register = function(user){
-      user.first_name = "";
-      user.last_name = "";
-      user.is_admin = false;
-      user.is_manager = false;
-      user.friends = [];
-      user.phonenumber = "";
       
-      Authentication.registerUser(user).then(function(s){
-        console.log(s);
-        if(s.status==201){
-          var auth = {};
-          auth.username = user.username;
-          auth.password = user.password;
-          $scope.login(auth);
-        }
-      }, function(e){console.log(e);});
+      if(user.password == user.confirm_password){
+        user.is_admin = false;
+        user.is_manager = false;
+        user.friends = [];
+        user.phonenumber = "";
+        
+        Authentication.registerUser(user).then(function(s){
+          if(s.status==201){
+            var auth = {};
+            auth.username = user.username;
+            auth.password = user.password;
+            $scope.login(auth);
+          }
+        }, function(e){console.log(e);});
+      }else{
+        console.log("Passwords did not match");
+        $scope.authError = true;
+      };  
     };
+      
+      
 
 }])
 
